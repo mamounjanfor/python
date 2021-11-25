925599e70d1a23c6b3a383c9edf288eac25dbfec
mabdalla <abdullamady@hotmail.com>
2021-11-25 13:41:52 +0100

    "Car game"

---
+++ car_game.py
@@ -0,0 +1,37 @@
+command = ""
+started = False
+stopped = True
+while True:
+          command = input("> ").lower()
+          if (command == 'help'):
+               print("""
+Start - to start the car
+Stop  - to stop the car
+quit  - to exit the program
+                    """)
+          elif (command == 'start'):
+              if started:
+                  print("Car already started ...")
+              else:
+                   started = True
+                   print("Car start to gooo....")
+
+          elif (command == 'stop'):
+
+              if started == True:
+                  started = False
+                  print("Car Stopped !")
+
+              else:
+                  print("Car already Stopped...!")
+
+          elif (command == 'quit'):
+               print("Program quiting....!")
+               exit()
+               break
+
+          else:
+               print("I do not undersatand this...")
+
+
+
